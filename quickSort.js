b2d5b587479c3c49f6aeebd3f617834f682a566f
Erfuuan <erfuuan@gmail.com>
2025-08-16 00:45:09 +0330

    add 2 new algorithm

---
+++ quickSort.js
@@ -0,0 +1,16 @@
+function quickSort(arr) {
+    if (arr.length <= 1) return arr;
+    let pivot=arr[arr.length-1]
+    let left=[]
+    let right=[]
+    for(i=0;i<arr.length-1;i++){
+        if(arr[i]<pivot){
+            left.push(arr[i])
+        }else{
+            right.push(arr[i])
+        }
+    }
+    return [...quickSort(left),pivot,...quickSort(right)]
+}
+
+console.log(quickSort([5, 3, 8, 4, 2]));
